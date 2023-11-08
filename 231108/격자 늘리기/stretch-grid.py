2d34b266cf3db4b3ade620a3f2ba7698dd58bbad
mjtime <97143894+mjtime@users.noreply.github.com>
2023-11-09 00:04:51 +0900

    Title: 격자 늘리기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231108/격자 늘리기/stretch-grid.py
@@ -0,0 +1,21 @@
+n, m, k = map(int, input().split())
+arr = [
+    input() for _ in range(n)
+]
+
+repeat = k//2
+for line in arr:
+    char = line[0]
+    start = 0
+    line_tmp = ''
+    for i in range(m):
+        if line[i] != char:
+            char = arr[i]
+            end = i
+            line_tmp += line[start:end]*k
+            start = i
+        if i == m-1:
+            end = m
+            line_tmp += line[start:end]*k
+            for j in range(k):
+                print(line_tmp)
