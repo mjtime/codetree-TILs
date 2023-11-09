c54017bd3317155abb294ef22fbe3b7ba0a6acae
mjtime <97143894+mjtime@users.noreply.github.com>
2023-11-10 03:02:03 +0900

    Title: 세 수의 최대 곱, Time: 143ms, Memory: 73MB, Status: Passed - CodeTree

--- 231109/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 231109/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -3,9 +3,7 @@ arr = list(map(int, input().split()))
 
 arr.sort()
 
-test_arr = []
-test_arr.append(arr[-1] * arr[-2] * arr[-3])
-test_arr.append(arr[0] * arr[1] * arr[2])
-test_arr.append(arr[0] * arr[1] * arr[-1])
+test1 = arr[-1] * arr[-2] * arr[-3]
+test2 = arr[0] * arr[1] * arr[-1]
 
-print(max(test_arr))
+print(max(test1, test2))
