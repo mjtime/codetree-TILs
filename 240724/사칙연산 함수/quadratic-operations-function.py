ca0b6c2c9cccc07610e9c974f3a62b20fbcc3ace
mjtime <97143894+mjtime@users.noreply.github.com>
2024-07-24 18:28:03 +0900

    Title: 사칙연산 함수, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

--- 240724/사칙연산 함수/quadratic-operations-function.py
+++ 240724/사칙연산 함수/quadratic-operations-function.py
@@ -1,21 +1,23 @@
 a, o, c = map(str, input().split())
 a = int(a)
 c = int(c)
-def cal(x, y, z):
-    result = 0
-    if y == '+':
-        result = x+z
-    elif y == '-':
-        result = x-z
-    elif y == '/':
-        result = x//z
-    elif y == '*':
-        result = x * z
-    return str(result)
 
-if o == '/' and c==0:
-    print()
-elif o not in ['+', '-', '*', '/']:
-    print("False")
+def plus(a, b):
+    return a + b
+def minus(a, b):
+    return a - b
+def times(a, b):
+    return a * b
+def divide(a, b):
+    return a // b
+
+if o == '+':
+    print(a, o, c, "=", plus(a, c))
+elif o == '-':
+    print(a, o, c, "=", minus(a, c))
+elif o == '*':
+    print(a, o, c, "=", times(a, c))
+elif o == '/':
+    print(a, o, c, "=", divide(a, c))
 else:
-    print(str(a), o, str(c), '=', cal(a, o, c))
+    print("False")
