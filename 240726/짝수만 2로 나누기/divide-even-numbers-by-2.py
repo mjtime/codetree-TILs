e763dae30406fcdd5f63c3a4f3a82e8fa13da7cc
mjtime <97143894+mjtime@users.noreply.github.com>
2024-07-26 00:23:56 +0900

    Title: 짝수만 2로 나누기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240726/짝수만 2로 나누기/divide-even-numbers-by-2.py
@@ -0,0 +1,12 @@
+n = int(input())
+_list = list(map(int, input().split()))
+
+# 값을 반환하지 않는 함수 사용
+def half(arr):
+    for i in arr:
+        if i % 2 == 0:
+            print(i//2, end=' ')
+        else:
+            print(i, end=' ')
+
+half(_list)
