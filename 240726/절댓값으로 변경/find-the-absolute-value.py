247f9bf73f4c672ac4cb7fd99cf1eb95db07a628
mjtime <97143894+mjtime@users.noreply.github.com>
2024-07-26 01:35:07 +0900

    Title: 절댓값으로 변경, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240726/절댓값으로 변경/find-the-absolute-value.py
+++ 240726/절댓값으로 변경/find-the-absolute-value.py
@@ -1,13 +1,15 @@
 # 절대값으로 변경, 값을 반환하지 않는 함수
 
 n = int(input())
-_list = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
 def change_abs(arr):
-    for i in arr:
-        if i < 0:
-            print(i*-1, end =' ')
-        else:
-            print(i, end =' ')
+    for i in range(n):
+        if arr[i] < 0:
+            arr[i] = arr[i]*-1
+    return
 
-change_abs(_list[:])
+change_abs(arr)
+
+for elem in arr:
+    print(elem, end =' ')
