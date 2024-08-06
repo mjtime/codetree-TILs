f0f3bd69f06fe66ca487cc4a34d43aee915cfbb4
mjtime <97143894+mjtime@users.noreply.github.com>
2024-08-06 22:07:19 +0900

    Title: 중앙값 계산 2, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240806/중앙값 계산 2/get-median-2.py
+++ 240806/중앙값 계산 2/get-median-2.py
@@ -1,11 +1,10 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-arr.sort()
-
 def find_mid(i):
     idx_mid = i // 2
-    print(arr[idx_mid], end=' ')
+    sorted_arr = sorted(arr[:i])
+    print(sorted_arr[idx_mid], end=' ')
 
 for i in range(n+1):
     if i % 2 != 0:
