2e12e52b493a9367c1e88d7703d1b61d59b282b2
mjtime <97143894+mjtime@users.noreply.github.com>
2024-08-06 22:54:10 +0900

    Title: Next Level, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240806/Next Level/next-level.py
+++ 240806/Next Level/next-level.py
@@ -1,12 +1,17 @@
 class User:
-    def __init__(self, name = "codetree", level=10):
+    def __init__(self, name = "", level=1):
         self.name = name
         self.level = level
 
 user1 = User()
+user1.name = "codetree"
+user1.level = 10
 print(f'user {user1.name} lv {user1.level}')
-name, level = tuple(map(str, input().split()))
+
+name, level = tuple(input().split())
+
+user2 = User()
 user1.name = name
-user1.level = level
+user1.level = int(level)
 
 print(f'user {user1.name} lv {user1.level}')
