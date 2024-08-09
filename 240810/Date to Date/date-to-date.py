cdea5a0f7f4d11ceaec6cb2c1a5fc1090b50a9bb
mjtime <97143894+mjtime@users.noreply.github.com>
2024-08-10 00:31:28 +0900

    Title: Date to Date, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240810/Date to Date/date-to-date.py
@@ -0,0 +1,14 @@
+# 윤년이 아닌 해라고 가정, 2월은 28일까지 존재
+m1, d1, m2, d2 = map(int, input().split())
+days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+day1 = 0
+day2 = 0
+for i in range(m1):
+    day1 += days[i]
+day1 += d1
+for i in range(m2):
+    day2 += days[i]
+day2 += d2
+
+print(day2 - day1)
