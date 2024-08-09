3358f78cb0a9f7c522ec0092c78a7c08d1e6542b
mjtime <97143894+mjtime@users.noreply.github.com>
2024-08-10 00:53:35 +0900

    Title: Date to Date, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240810/Date to Date/date-to-date.py
+++ 240810/Date to Date/date-to-date.py
@@ -4,11 +4,12 @@ days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 day1 = 0
 day2 = 0
-for i in range(m1):
+for i in range(m1-1):
     day1 += days[i]
 day1 += d1
-for i in range(m2):
+
+for i in range(m2-1):
     day2 += days[i]
 day2 += d2
 
-print(day2 - day1)
+print(day2 - day1 + 1)
