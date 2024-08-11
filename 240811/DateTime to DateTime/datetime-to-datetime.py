401968842f9bf21dd06fc5f4b6aa2d104eab8a5d
mjtime <97143894+mjtime@users.noreply.github.com>
2024-08-11 21:38:38 +0900

    Title: DateTime to DateTime, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240811/DateTime to DateTime/datetime-to-datetime.py
+++ 240811/DateTime to DateTime/datetime-to-datetime.py
@@ -1,21 +1,9 @@
 start_time = 11
 d2, h2, m2 = map(int, input().split())
 
-def check_day():
-    if start_time<d2:
-        return True
-    elif start_time==d2 and start_time<h2:
-        return True
-    elif start_time==d2 and start_time==h2 and start_time<=m2 :
-        return True
-    else:
-        return False
-# 1day = 24 hour = 1440min
-if check_day():
-    day = d2 - start_time
-    hour = h2 - start_time
-    mins = m2 - start_time
+mins = (d2 * 24 * 60 + h2 * 60 + m2) - (start_time * 24 * 60 + start_time * 60 + start_time)
 
-    print(day*24*60 + hour*60 + mins)
-else:
+if mins < 0:
     print(-1)
+else:
+    print(mins)
