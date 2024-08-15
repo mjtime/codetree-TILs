4e6c55bd9cedcd1c85d0610cb3d5d9547aace714
mjtime <97143894+mjtime@users.noreply.github.com>
2024-08-16 02:34:46 +0900

    Title: 왔다 갔던 구역 2, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240816/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240816/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,42 +1,33 @@
 n = int(input())
+# offset => n = 100 번씩 x=10인 경우 100x10 = 1000
+# lenth => +-값 보정, -1000 ~ 1000 => 0 ~ 2000
 arr_len = 2001
 arr = [0 for _ in range(arr_len)]
 now = 1000
-# maxs = 0
-# mins = 2000
+
 for _ in range(n):
     distance, dic = input().split()
     distance = int(distance)
-    # if dic == 'R':
-    #     start = now
-    #     end = distance + 1000 + 1
-    # else:
-    #     start = 1000 - distance
-    #     end = now + 1
+
     if dic =='R':
         distance = now + distance
     else:
         distance = now - distance
+    
     start = min(now, distance)
     end = max(now, distance)
     
+    # 지점일 경우 end + 1,
+    # start, end가 점이라고 할때
+    # 구간이기 때문에 점과 점사이에 +1해준다고 생각
+    # start = 1, end = 3인 경우 => 1~2, 2~3 총 두곳에 +1
     arr[start:end] = [x+1 for x in arr[start:end]]
     
+    # R일 경우 오른쪽 방향으로 설정(now -> end 바라봄)
+    # L일 경우 왼쪽 방향으로 설정(start <- now 바라봄)
     if dic == 'R':
         now = end
     else:
         now = start
-    # print(dic, start, end, now)
-    # print(arr[989:1003])
-    # mins = min(mins, start)
-    # maxs = max(maxs, end)
-# print(mins, maxs)
-# print(arr[mins:maxs])
-# cnt = 0
-# check = 0
-# for num in arr[mins:maxs+1]:
-#     if num > 1 and check != num:
-#         cnt += 1
-#     check = num
-# print(cnt)
+#2번 이상 지나간 구간 = 전체 - 1번 이하로 지나간 구간
 print(arr_len-arr.count(0)-arr.count(1))
