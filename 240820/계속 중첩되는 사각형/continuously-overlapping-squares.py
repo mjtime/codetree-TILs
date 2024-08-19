8cc32033d0241a0f8844e4f37752bb209435a637
mjtime <97143894+mjtime@users.noreply.github.com>
2024-08-20 01:32:10 +0900

    Title: 계속 중첩되는 사각형, Time: 111ms, Memory: 28MB, Status: Passed - CodeTree

--- 240820/계속 중첩되는 사각형/continuously-overlapping-squares.py
+++ 240820/계속 중첩되는 사각형/continuously-overlapping-squares.py
@@ -9,7 +9,7 @@ def drow(x1, y1, x2, y2, num):
 
 for i in range(n):
     x1, y1, x2, y2 = map(lambda x:int(x)+OFFSET, input().split())
-    drow(x1, y1, x2, y2,(i+1)//2)
+    drow(x1, y1, x2, y2,(i+2)%2)
 
 area = 0
 for i in range(200):
