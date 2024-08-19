7d9e1591ba30115aebf37228add0b20ee7493ca0
mjtime <97143894+mjtime@users.noreply.github.com>
2024-08-20 01:48:58 +0900

    Title: 연속되는 수 2, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 240820/연속되는 수 2/continuous-number2.py
+++ 240820/연속되는 수 2/continuous-number2.py
@@ -1,13 +1,16 @@
 n = int(input())
-max_cnt = -1
+max_cnt = 1
 cnt = 1
 before = -1
-for _ in range(n):
+for i in range(n):
     num = int(input())
+    if i == 0:
+        before = num
+        continue
     if num == before:
         cnt += 1
     else:
         before = num
-        max_cnt = max(max_cnt, cnt)
         cnt = 1
+    max_cnt = max(max_cnt, cnt)
 print(max_cnt)
