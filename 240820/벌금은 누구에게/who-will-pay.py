e0ad21bd2cc93f2f141e768cc313d2cf81a28617
mjtime <97143894+mjtime@users.noreply.github.com>
2024-08-20 22:15:56 +0900

    Title: 벌금은 누구에게, Time: 134ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240820/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,9 @@
+n, m, k = map(int, input().split())
+students = [0 for _ in range(n)]
+ans = -1
+for _ in range(m):
+    num = int(input())
+    students[num - 1] += 1
+    if k in students:
+        ans = students.index(k)+1
+print(ans)
