845428ef6ce69e6d0c0fa7d6f1d2167db29a3b24
mjtime <97143894+mjtime@users.noreply.github.com>
2024-08-20 21:33:36 +0900

    Title: 만나는 그 순간, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240820/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,28 @@
+n, m = map(int, input().split())
+arr_a = [0]
+arr_b = [0]
+
+def go_left(arr, num):
+    for i in range(num):
+        arr.append(arr[len(arr)-1] - 1)
+
+def go_right(arr, num):
+    for i in range(num):
+        arr.append(arr[len(arr)-1] + 1)
+
+def input_move(arr, n):
+    for i in range(n):
+        dire, go = input().split()
+        go = int(go)
+        if dire == 'L':
+            go_left(arr, go)
+        else:
+            go_right(arr, go)
+
+input_move(arr_a, n)
+input_move(arr_b, m)
+
+for i in range(1, min(len(arr_a), len(arr_b))):
+    if arr_a[i] == arr_b[i]:
+        print(i)
+        break
