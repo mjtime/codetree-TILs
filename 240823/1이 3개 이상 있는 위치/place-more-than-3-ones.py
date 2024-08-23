5b8db8b0b69f5b81114252c844cdfebc209950bf
mjtime <97143894+mjtime@users.noreply.github.com>
2024-08-23 23:51:14 +0900

    Title: 1이 3개 이상 있는 위치, Time: 186ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240823/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -0,0 +1,25 @@
+# [v] n*n 격자
+# [v] dxs, dys
+# [] 상하좌우 1이 3개 이상인 곳 카운트
+# [] 상하좌우 확인시 격자 안 벗어나는지 확인 후 연산
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+ans = 0
+
+for x in range(n):
+    for y in range(n):
+        cnt = 0
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if in_range(nx, ny) and arr[nx][ny] == 1:
+                cnt += 1
+        if cnt >= 3:
+            ans += 1
+
+print(ans)
