345164e8e7d8b59e813b844b26cd48ed9de64f71
mjtime <97143894+mjtime@users.noreply.github.com>
2024-08-25 23:55:28 +0900

    Title: 두 직사각형, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240825/두 직사각형/two-rectangles.py
+++ 240825/두 직사각형/two-rectangles.py
@@ -2,7 +2,7 @@ x1, y1, x2, y2 = map(int, input().split())
 a1, b1, a2, b2 = map(int, input().split())
 
 check = True
-if x2<a1 or a2 <x1 or b2<y1 or y2< b2:
+if x2<a1 or a2 <x1 or b2<y1 or y2< b1:
     check = False
 
 if check:
