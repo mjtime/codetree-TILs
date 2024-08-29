35d663a0733dd1d97656426f3096fbf56e1f83ba
mjtime <97143894+mjtime@users.noreply.github.com>
2024-08-30 01:16:49 +0900

    Title: 최고의 13위치 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240830/최고의 13위치 2/best-place-of-13-2.py
+++ 240830/최고의 13위치 2/best-place-of-13-2.py
@@ -4,13 +4,27 @@ n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 max_num_coin = -sys.maxsize
 
-for i in range(n - 1):
-    num_coin = 0
+check = False
+for i in range(n):
+    num_coin1, num_coin2, num_coin3 = 0, 0, 0
     for j in range(n - 2):
-        num_coin += arr[i][j] + arr[i][j+1] + arr[i][j+2]
+        num_coin1 = (arr[i][j] + arr[i][j+1] + arr[i][j+2])
+        if n-2-j>3 and not check:
+            for m in range(j+3, n-2):
+                num_coin3 = (arr[i][m] + arr[i][m+1] + arr[i][m+2])
+                max_num_coin = max(max_num_coin, num_coin1 + num_coin2)
+                if m == n-2:
+                    check = True
+        else:
+            for k in range(i+1, n):
+                for l in range(n - 2):
+                    num_coin2 = (arr[k][l] + arr[k][l+1] + arr[k][l+2])
+                    # print(num_coin1, num_coin2)
+                    max_num_coin = max(max_num_coin, num_coin1 + num_coin2)
+                if i == n:
+                    check = False
+        
 
-    for k in range(n - 2):
-        num_coin += arr[i+1][k] + arr[i+1][k+1] + arr[i+1][k+2]
-    max_num_coin = max(max_num_coin, num_coin)
+
 
 print(max_num_coin)
