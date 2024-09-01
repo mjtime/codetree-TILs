b9c0bd86c6886e84362de286a222cbd4c0c2d1bc
mjtime <97143894+mjtime@users.noreply.github.com>
2024-09-02 01:21:29 +0900

    Title: 아름다운 수열, Time: 181ms, Memory: 26MB, Status: Passed - CodeTree

--- 240902/아름다운 수열/beautiful-sequence.py
+++ 240902/아름다운 수열/beautiful-sequence.py
@@ -3,55 +3,30 @@
 # beautiful sequence B is a state where the same operation is applied to each element, or the sequence itself.
 # => ex) B[4, 6, 7] -> [3, 5, 7] or [5, 3, 6] or [4, 6, 7], etc...
 # //print// how many beautiful squence are in A ?
-import itertools
+
+# algorithm
+# 1) A의 부분수열과 B수열 오름차순 정렬
+# 2) temp_arr를 B수열 만큼 만듦
+# 3) temp_arr에 '1)'의 각 자리수를 뺀 값을 담음
+# 4) temp의 모든 값이 같으면 아름다운 수열
 
 n = int(input())
 arr_n = [int(input()) for _ in range(n)]
 m = int(input())
 arr_m = [int(input()) for _ in range(m)]
 beautiful_idx = []
-max_n = max(arr_n)
-min_n = min(arr_n)
-
-# def permute(arr):
-#     result = []
-#     if len(arr) == 1:
-#         return [arr]
-    
-#     for i in range(len(arr)):
-#         remaining_elements = arr[:i] + arr[i+1:]
-#         for p in permute(remaining_elements):
-#             result.append([arr[i]]+p)
-#     return result
-
-
-for i in range(n - m + 1):
-    arr_permutaion1 = list(itertools.permutations(arr_m))
-    for perm in arr_permutaion1:
-        if tuple(arr_n[i:i+m]) == perm:
+sorted_m = sorted(arr_m)
+for i in range(n-m+1):
+    sorted_n = sorted(arr_n[i:i+m])
+    for j in range(m):
+        if j == 0:
+            diff = sorted_n[j]-sorted_m[j]
+        else:
+            if diff != sorted_n[j]-sorted_m[j]:
+                break
+        if j == m-1:
             beautiful_idx.append(i+1)
-    
-    temp_arr2 = arr_m
-    while True:
-        if max(temp_arr2) >= max_n:
-            break
-        temp_arr2 = [x+1 for x in temp_arr2]
-        arr_permutaion2 = list(itertools.permutations(temp_arr2))
-        for perm in arr_permutaion2:
-            if tuple(arr_n[i:i+m]) == perm:
-                beautiful_idx.append(i+1)
-    
-    temp_arr3 = arr_m
-    while True:
-        if min(temp_arr3) <= min_n:
-            break
-        temp_arr3 = [x-1 for x in temp_arr3]
-        arr_permutaion3 = list(itertools.permutations(temp_arr3))
-        for perm in arr_permutaion3:
-            if tuple(arr_n[i:i+m]) == perm:
-                beautiful_idx.append(i+1)
 
 print(len(beautiful_idx))
-beautiful_idx.sort()
 for idx in beautiful_idx:
     print(idx)
