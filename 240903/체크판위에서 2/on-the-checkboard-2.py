4185179bb3482086b530b443da085cfb06515443
mjtime <97143894+mjtime@users.noreply.github.com>
2024-09-03 00:22:33 +0900

    Title: 체크판위에서 2, Time: 113ms, Memory: 25MB, Status: Passed - CodeTree

--- 240903/체크판위에서 2/on-the-checkboard-2.py
+++ 240903/체크판위에서 2/on-the-checkboard-2.py
@@ -3,8 +3,9 @@ r,c = map(int, input().split())
 arr = [list(input().split()) for _ in range(r)]
 
 cnt = 0
-for i in range(r):
-    for j in range(c):
+# 시작점(0, 0) 범위 제외
+for i in range(1, r):
+    for j in range(1, c):
         # 마지막(r, c)는 도착지점이라 범위 제외
         for k in range(i+1, r-1):
             for l in range(j+1, c-1):
