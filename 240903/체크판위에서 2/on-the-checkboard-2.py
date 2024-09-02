988ea96b5adda95205f6e075e2177013406a0610
mjtime <97143894+mjtime@users.noreply.github.com>
2024-09-03 00:17:14 +0900

    Title: 체크판위에서 2, Time: 114ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240903/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,15 @@
+# (0, 0)에서 (r, c)로 이동
+r,c = map(int, input().split())
+arr = [list(input().split()) for _ in range(r)]
+
+cnt = 0
+for i in range(r):
+    for j in range(c):
+        # 마지막(r, c)는 도착지점이라 범위 제외
+        for k in range(i+1, r-1):
+            for l in range(j+1, c-1):
+                if arr[0][0] != arr[i][j] and \
+                   arr[i][j] != arr[k][l] and \
+                   arr[k][l] != arr[r-1][c-1]:
+                    cnt += 1
+print(cnt)
