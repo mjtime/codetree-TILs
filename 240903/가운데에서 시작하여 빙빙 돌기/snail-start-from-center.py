49626af7f1a513a3940e2018c075bfe9399d8595
mjtime <97143894+mjtime@users.noreply.github.com>
2024-09-03 22:39:00 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 140ms, Memory: 25MB, Status: Passed - CodeTree

--- 240903/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 240903/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -1,3 +1,4 @@
+# 2번 방향전환 마다 직진 수 +1 증가
 n = int(input())
 arr = [[0]*n for _ in range(n)]
 x, y = n//2, n//2
@@ -8,41 +9,29 @@ def in_range(x, y):
     return 0<=x<n and 0<=y<n
 
 arr[x][y] = 1
-k = 1
-cnt = 0
+move  = 1
+change_dir_cnt = 0
 num = 2
 end = True
 while end:
-    # print('k', k)
-    for _ in range(k):
+    for _ in range(move):
         nx = x+dxs[dir_num]
         ny = y+dys[dir_num]
         if in_range(nx, ny):
             x, y = nx, ny
-        # print(num, x, y)
             arr[x][y] = num
             num += 1
         if num > n*n:
             end = False
             break
+    #move만큼 직진 후 1회 -90도 회전
     dir_num = (dir_num+3)%4
-    cnt += 1
-    if cnt == 2:
-        k+=1
-        cnt = 0
-# for i in range(1, n*n - 1):
-#     nx = x+dxs[dir_num]
-#     ny = y+dys[dir_num]
-#     x, y = nx, ny
-#     arr[x][y] = i+1
-#     cnt += 1
+    change_dir_cnt += 1
 
-    # if not in_range(nx, ny) or arr[nx][ny] != 0:
-    #     dir_num = (dir_num+3)%4
-    #     nx = x+dxs[dir_num]
-    #     ny = y+dys[dir_num]
-    # x, y = nx, ny
-    # arr[x][y] = i+2
+    #2번 방향 전환 후 이동수 1 증가
+    if change_dir_cnt == 2:
+        move+=1
+        change_dir_cnt = 0
 
 for i in range(n):
     for j in range(n):
