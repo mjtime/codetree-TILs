91f4d0d3f41824fa049fbad14222b134c22d9398
mjtime <97143894+mjtime@users.noreply.github.com>
2024-09-09 19:58:24 +0900

    Title: 겹치지 않게 선분 고르기, Time: 5096ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240909/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 240909/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -6,12 +6,10 @@ ans = 0
 def overlap(curr_idx, base_line):
     curr_x1, curr_x2 = lines[curr_idx]
     base_x1, base_x2 = lines[base_line]
-    if curr_x1 <= base_x1 <= curr_x2:
-        return True
-    elif curr_x1 <= base_x2 <= curr_x2:
-        return True
-    else:
-        return False
+    # print(curr_x1, curr_x2, base_x1, base_x2)
+    # 선분이 겹치는 조건: 하나의 선분의 시작점이 다른 선분의 끝점보다 작거나 같고,
+    # 끝점이 다른 선분의 시작점보다 크거나 같으면 겹친다.
+    return not(curr_x2 < base_x1 or curr_x1 > base_x2)
 
 def check_lines(curr_idx):
     for base_line_idx in select_lines:
@@ -23,8 +21,9 @@ def choose(curr_num):
     global ans
     if curr_num >= n:
         ans = max(ans, len(select_lines))
-        # print(select_lines)
-        # print("==================")
+        # if len(select_lines)>2:
+        #     print(select_lines)
+        #     print("==================")
         return
     
     for curr_idx in range(n):
@@ -36,6 +35,8 @@ def choose(curr_num):
             choose(curr_num + 1)
     
     return
-    
+
 choose(0)
 print(ans)
+
+# print(check_lines(4))
